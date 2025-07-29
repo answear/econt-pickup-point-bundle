aab23a429b4f2bce170b4cfcf54a01c536b6708a
Lukasz Falda <lukasz.falda@wearco.pl>
2025-07-29 09:25:40 +0200

    Get response method

--- src/Command/AbstractCommand.php
+++ src/Command/AbstractCommand.php
@@ -4,13 +4,29 @@ declare(strict_types=1);
 
 namespace Answear\EcontBundle\Command;
 
+use Answear\EcontBundle\Client\Client;
+use Answear\EcontBundle\Client\RequestTransformer;
 use Answear\EcontBundle\Exception\MalformedResponseException;
+use Answear\EcontBundle\Request\Request;
 use Answear\EcontBundle\Response\ErrorResponse;
 use Psr\Http\Message\ResponseInterface;
 use Webmozart\Assert\Assert;
 
 abstract readonly class AbstractCommand
 {
+    public function __construct(
+        protected Client $client,
+        protected RequestTransformer $transformer
+    ) {
+    }
+
+    public function getResponse(Request $request): ResponseInterface
+    {
+        $httpRequest = $this->transformer->transform($request);
+
+        return $this->client->request($httpRequest);
+    }
+
     protected function getBody(ResponseInterface $response): array
     {
         try {
